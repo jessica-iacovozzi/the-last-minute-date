2f3744d02db15d0438d24e539482d67cb13c7e3f
Giovanni Reid <giovanni-mtl@hotmail.com>
2022-11-22 17:47:39 -0500

    finished the seed so far with the events and the users

--- db/seeds.rb
+++ db/seeds.rb
@@ -76,6 +76,7 @@ time1 = Time.now
 time2 = time1 + (4 * 60 * 60) # plus 4 hours
 time3 = time1 + (2 * 60 * 60) # plus 2 hours
 
+# Formatting the dates -> Tue, Nov 22, 6pm
 time1.strftime("%a, %b %e, %-I%P")
 time2.strftime("%a, %b %e, %-I%P")
 time3.strftime("%a, %b %e, %-I%P")
@@ -84,69 +85,74 @@ puts 'Creating Event...'
 salsa = {
   title: 'Salsa Latin Groove Party At Club 6/49',
   date: time1,
-  category: 'Salsa',
+  category: 'Arts & Theatre',
   address: '1112 Saint-Catherine St W, Montreal, Quebec',
+  description: 'buggie woogy and let loose',
   price: '5',
   artist: 'Myriam and Louis',
   picture_url: 'https://www.danceus.org/events/images/166897830863316/salsa-groove-party-at-club--cover.png'
 }
-
-dancing = {
-  title: '1909 Av. des Canadiens-de-Montréal, Montreal, Quebec',
-  date: time2,
-  category: 'Salsa',
-  address: '1112 Saint-Catherine St W, Montreal, Quebec',
-  price: '5',
-  artist: 'Myriam and Louis',
-  picture_url: 'https://www.danceus.org/events/images/166897830863316/salsa-groove-party-at-club--cover.png'
-}
-
-concert = {
-  title: 'Salsa Latin Groove Party At Club 6/49',
-  date: time3,
-  category: 'Salsa',
-  address: '1112 Saint-Catherine St W, Montreal, Quebec',
-  price: '5',
-  artist: 'Myriam and Louis',
-  picture_url: 'https://www.danceus.org/events/images/166897830863316/salsa-groove-party-at-club--cover.png'
-}
-
 hockey = {
-  title: 'Salsa Latin Groove Party At Club 6/49',
+  title: 'Montreal Canadiens vs. Nashville Predators',
+  date: time2,
+  category: 'Sports',
+  address: 'Centre Bell, 1909 Av. des Canadiens-de-Montréal, Montréal, QC',
+  description: 'Watch your favorite game',
+  price: '287',
+  artist: 'Nashville vs Montreal Canadiens',
+  picture_url: 'https://b.fssta.com/uploads/application/nhl/venues/Bell-Centre.vresize.2240.1260.medium.1.jpg'
+}
+Comedy = {
+  title: 'Patrick Norman',
   date: time3,
-  category: 'Salsa',
-  address: '1112 Saint-Catherine St W, Montreal, Quebec',
-  price: '5',
-  artist: 'Myriam and Louis',
-  picture_url: 'https://www.danceus.org/events/images/166897830863316/salsa-groove-party-at-club--cover.png'
+  category: 'Arts & Theatre',
+  address: 'L`Étoile Banque Nationale
+  6000 Boul. de Rome, Brossard, Quebec',
+  description: "Patrick Norman will be performing near you at L'Étoile on Thursday 22 December 2022 as part of their tour, and are scheduled to play 1 concert across 1 country in 2022-2023.",
+  price: '45',
+  artist: 'Patrick Norman',
+  picture_url: 'https://www.nvrc.ca/sites/default/files/styles/section_landing_banner/public/default_images/theatre-placeholder-banner.jpg?itok=b1b9eQja'
+}
+skeggs_concert = {
+  title: 'SKEGSS',
+  date: time3,
+  category: 'Concerts',
+  address: 'Théâtre Corona Montreal, Canada',
+  description: '',
+  price: '64',
+  artist: 'SKEGSS, Sir Chloe and Adam Newling',
+  picture_url: 'https://images.thebrag.com/tmn/uploads/Skegss-Press-Shot-2019.png'
 }
 
-ball = {
-  title: '1909 Av. des Canadiens-de-Montréal, Montreal, Quebec',
+Symphorien = {
+  title: 'Symphorien',
   date: time1,
-  category: 'Salsa',
-  address: '1112 Saint-Catherine St W, Montreal, Quebec',
-  price: '5',
-  artist: 'Myriam and Louis',
+  category: 'Arts & Theatre',
+  address: "L'Etoile Brossard, Canada",
+  description: 'The main character, Symphorien, is a naive but sympathetic fellow, who is the janitor for this guest house run by Mrs. Sylvain, a single yet bumbling lady who runs the house in a strict, efficient way.',
+  price: '55',
+  artist: 'Symphorien',
   picture_url: 'https://www.danceus.org/events/images/166897830863316/salsa-groove-party-at-club--cover.png'
 }
 
 yoga = {
-  title: 'Salsa Latin Groove Party At Club 6/49',
+  title: "Les Shirley - More is More (Lancement d'album)",
   date: time2,
-  category: 'Salsa',
-  address: '1112 Saint-Catherine St W, Montreal, Quebec',
-  price: '5',
-  artist: 'Myriam and Louis',
+  category: 'Concerts',
+  address: 'Fairmount Theatre
+  Montreal, Canada',
+  description: 'Sweat at the show not before',
+  price: '40',
+  artist: 'Les Shirley',
   picture_url: 'https://www.danceus.org/events/images/166897830863316/salsa-groove-party-at-club--cover.png'
 }
 
-# Messages Seed
-# Message.destroy_all
-# puts "destroyed all messages"
+Messages Seed
+Message.destroy_all
+puts "destroyed all messages"
 
-# Conversation.destroy_all
-# puts "destroyed all conversations"
+Conversation.destroy_all
+puts "destroyed all conversations"
 
 # User.destroy_all
 # puts "destroyed all users"
@@ -154,19 +160,19 @@ yoga = {
 # User.create!(email: "j@j.com", password: "123456")
 # User.create!(email: "e@e.com", password: "123456")
 
-# Conversation.create!
+Conversation.create!
 
-# Message.create!(content: "Hey", sender: User.first, receiver: User.last, conversation: Conversation.first)
-# Message.create!(content: "What's up?", sender: User.first, receiver: User.last, conversation: Conversation.first)
+Message.create!(content: "Hey", sender: User.first, receiver: User.last, conversation: Conversation.first)
+Message.create!(content: "What's up?", sender: User.first, receiver: User.last, conversation: Conversation.first)
 
-# Message.create!(content: "Yo", sender: User.last, receiver: User.first, conversation: Conversation.first)
-# Message.create!(content: "Not much, you?", sender: User.last, receiver: User.first, conversation: Conversation.first)
+Message.create!(content: "Yo", sender: User.last, receiver: User.first, conversation: Conversation.first)
+Message.create!(content: "Not much, you?", sender: User.last, receiver: User.first, conversation: Conversation.first)
 
-# users = User.create([{ first_name: "Jessica",
-#                        username: "theoneandonlyjess",
-#                        age: 26,
-#                        description: "I love R&B. My favourite artists are Jessie Reyez and Kehlani",
-#                        picture_url: "https://images.unsplash.com/photo-1628015081036-0747ec8f077a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Z2lybHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
-#                        tags: ["R&B"],
-#                        email: "iacovozzi.jessica@gmail.com",
-#                        password: "123456" }])
+users = User.create([{ first_name: "Jessica",
+                       username: "theoneandonlyjess",
+                       age: 26,
+                       description: "I love R&B. My favourite artists are Jessie Reyez and Kehlani",
+                       picture_url: "https://images.unsplash.com/photo-1628015081036-0747ec8f077a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Z2lybHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+                       tags: ["R&B"],
+                       email: "iacovozzi.jessica@gmail.com",
+                       password: "123456" }])
