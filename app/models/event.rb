c4e6ea6670226b93675069755e4aa95eda19b8a8
jessica iacovozzi <iacovozzi.jessica@gmail.com>
2022-11-22 12:40:13 -0500

    started working on the seed, switching to update schema

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,5 @@ class Event < ApplicationRecord
   has_many :tickets
   has_many :messages
 
-  validates :title, :date, :category, :address, :description, :price, :artist, :picture_url, presence: true
+  # validates :title, :date, :category, :address, :description, :price, :artist, :picture_url, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# users = User.create([{ first_name: "Jessica",
+#                        username: "theoneandonlyjess",
+#                        age: 26,
+#                        description: "I love R&B. My favourite artists are Jessie Reyez and Kehlani",
+#                        picture_url: "https://images.unsplash.com/photo-1628015081036-0747ec8f077a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Z2lybHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+#                        tags: ["R&B"],
+#                        email: "iacovozzi.jessica@gmail.com",
+#                        password: "123456" }])
+
+User.create!(email: "j@j.com", password: "123456")
+User.create!(email: "e@e.com", password: "123456")
