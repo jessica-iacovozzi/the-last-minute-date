47633e8fed86f79cdfa0198bee949ea7aa84dec4
jessica iacovozzi <iacovozzi.jessica@gmail.com>
2022-11-22 15:32:18 -0500

    added message content validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ApplicationRecord
   has_many :tickets
+  has_many :users, through: :tickets
 
   # validates :title, :date, :category, :address, :description, :price, :artist, :picture_url, presence: true
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,6 @@ class Message < ApplicationRecord
   belongs_to :conversation
   belongs_to :sender, class_name: "User"
   belongs_to :receiver, class_name: "User"
+
+  validates :content, presence: true, length: { minimum: 2 }
 end
