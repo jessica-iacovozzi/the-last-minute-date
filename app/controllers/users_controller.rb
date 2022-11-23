3a7a938aeced9cf9562b70a41a29efaf53791972
jessica iacovozzi <iacovozzi.jessica@gmail.com>
2022-11-23 14:06:46 -0500

    craeted route and controller action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,5 @@
 class UsersController < ApplicationController
+  def show
+    @my_profile = User.find(id: current_user)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+<<<<<<< Updated upstream
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+=======
+  get '/my_tickets', to: 'tickets#my_tickets'
+  get '/my_profile', to: 'users#show'
+>>>>>>> Stashed changes
 end
