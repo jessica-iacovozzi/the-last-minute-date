a93cd8c8a2e31843b0c2ba043c6054d41098b0d1
jessica iacovozzi <iacovozzi.jessica@gmail.com>
2022-11-23 16:07:07 -0500

    added association to users to add events to profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
     @my_profile = current_user
+    @my_events = @my_profile.events
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -438,12 +438,17 @@ Message.create!(
   conversation: Conversation.first
 )
 
-users = User.create([{ first_name: "Jessica",
-                       username: "theoneandonlyjess",
-                       age: 26,
-                       description: "I love R&B. My favourite artists are Jessie Reyez and Kehlani",
-                       picture_url: "https://images.unsplash.com/photo-1628015081036-0747ec8f077a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Z2lybHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
-                       tags: ["R&B"],
-                       email: "iacovozzi.jessica@gmail.com",
-                       password: "123456" }])
-puts 'All Done!'
+Ticket.create!(
+  event_id: 9,
+  user_id: 9
+)
+
+# users = User.create([{ first_name: "Jessica",
+#                        username: "theoneandonlyjess",
+#                        age: 26,
+#                        description: "I love R&B. My favourite artists are Jessie Reyez and Kehlani",
+#                        picture_url: "https://images.unsplash.com/photo-1628015081036-0747ec8f077a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Z2lybHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+#                        tags: ["R&B"],
+#                        email: "iacovozzi.jessica@gmail.com",
+#                        password: "123456" }])
+# puts 'All Done!'
