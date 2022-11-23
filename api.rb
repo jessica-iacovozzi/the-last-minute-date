2b7389fd9445b6cc451c04464472b8b08244f55c
Hugo Dulac <hugodulaclemelin@gmail.com>
2022-11-23 15:13:34 -0500

    trying to get shows for a date

--- api.rb
+++ api.rb
@@ -1,1 +1,9 @@
-puts "Hello"
+require "json"
+require "open-uri"
+require "nokogiri"
+# base url = https://app.ticketmaster.com/{package}/{version}/{resource}.json?apikey=**{cMCr5449SSuQYbG9LSGVtssAhx68HA6l}
+url = "https://app.ticketmaster.com/discovery/v2/events.json?stateCode=QC&apikey=#{ENV['TICKET_MASTER_KEY']}"
+user_serialized = URI.open(url).read
+events = JSON.parse(user_serialized)
+
+puts events
