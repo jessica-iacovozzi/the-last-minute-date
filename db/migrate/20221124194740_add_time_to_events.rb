e08be7f5efc7faf22a07e166d51bd1fb0faffea5
Giovanni Reid <giovanni-mtl@hotmail.com>
2022-11-24 14:48:09 -0500

    updated the migration

--- db/migrate/20221124194740_add_time_to_events.rb
+++ db/migrate/20221124194740_add_time_to_events.rb
@@ -1,6 +1,6 @@
 class AddTimeToEvents < ActiveRecord::Migration[7.0]
   def change
-    add_column :events, :time, :integer
+    add_column :events, :time, :string
     add_column :events, :city, :string
     add_column :events, :venue, :string
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_24_185400) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_24_194740) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -58,7 +58,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_24_185400) do
     t.string "picture_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "time"
+    t.string "time"
     t.string "city"
     t.string "venue"
   end
