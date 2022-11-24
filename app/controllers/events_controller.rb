6a3390a8a3e95832351bb37a981feee5bcf067d6
Eva Roux <114696769+Evarouux@users.noreply.github.com>
2022-11-24 12:40:18 -0500

    creating the mapbox

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,15 @@
 class EventsController < ApplicationController
+
+  def index
+    @events = Event.all
+
+    @markers = @events.geocoded.map do |event|
+      {
+        lat: event.latitude,
+        lng: event.longitude
+        info_window: render_to_string(partial: "info_window", locals: {event: event})
+        image_url: helpers.asset_url("pin.png")
+      }
+    end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_23_173639) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_24_164922) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -58,6 +58,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_23_173639) do
     t.string "picture_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "messages", force: :cascade do |t|
