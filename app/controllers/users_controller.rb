367eb7ae5cd08f26ddfc4caf4c419f5fee6732ed
jessica iacovozzi <iacovozzi.jessica@gmail.com>
2022-11-24 17:13:22 -0500

    removed @my_events in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def my_profile
     @my_profile = current_user
-    @my_events = @my_profile.events
+    # @my_events = @my_profile.events
   end
 end
