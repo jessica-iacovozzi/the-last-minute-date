e29ef0e80825bd3ad4f52af410fb84725499632c
Giovanni Reid <giovanni-mtl@hotmail.com>
2022-11-25 16:48:31 -0500

    was able to style the chatroom

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,7 +8,8 @@ class MessagesController < ApplicationController
     if @message.save
       ConversationChannel.broadcast_to(
         @conversation,
-        render_to_string(partial: "conversations/message", locals: { message: @message })
+        message: render_to_string(partial: "conversations/message", locals: { message: @message }),
+        sender_id: @message.sender.id
       )
       head :ok
     else
--- app/models/message.rb
+++ app/models/message.rb
@@ -4,4 +4,7 @@ class Message < ApplicationRecord
   belongs_to :receiver, class_name: "User"
 
   validates :content, presence: true, length: { minimum: 2 }
+  def sender?(a_sender)
+    sender.id == a_sender.id
+  end
 end
