ce0e5243d8adbe8976a2be9b2401e9a58e2b3987
Eva Roux <114696769+Evarouux@users.noreply.github.com>
2022-11-25 15:19:53 -0500

    working buy tivcket methods

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,5 +1,24 @@
 class TicketsController < ApplicationController
   def my_tickets
-    @my_tickets = current_user.events
+    @my_tickets = current_user.events.order(date: :desc)
+  end
+
+  def buy_ticket
+    @ticket = Ticket.new()
+    @event = Event.find(params[:id])
+    @ticket.user = current_user
+    @ticket.event = @event
+
+    if @ticket.save
+      redirect_to my_tickets_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def params_ticket
+    params.require(:ticket).permit(:user_id, :event_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
 
   resources :events, only: %i[show]
 
+  resources :events, only: %i[show]
+
+  get '/events/:id/tickets', to: 'tickets#buy_ticket', as: :buy_ticket
   get '/my_tickets', to: 'tickets#my_tickets'
   get '/my_profile', to: 'users#my_profile'
 end
