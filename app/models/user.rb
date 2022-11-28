8d3cb77f84f06060767e9055a4cece371f240bd0
jessica iacovozzi <iacovozzi.jessica@gmail.com>
2022-11-28 11:42:31 -0500

    removed receiver id in user model and commented out api_call in seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :sent_messages, class_name: "Message", foreign_key: :sender_id, dependent: :destroy
-  # has_many :received_messages, class_name: "Message", foreign_key: :receiver_id, dependent: :destroy
   has_many :conversations, through: :sent_messages, dependent: :destroy
   has_many :tickets, dependent: :destroy
   has_many :events, through: :tickets
