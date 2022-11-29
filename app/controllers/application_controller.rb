22e3a4411fb29ca58f14b036459d4ca2e5a82601
jessica iacovozzi <iacovozzi.jessica@gmail.com>
2022-11-29 11:40:50 -0500

    added parameters on sign up and fixed image view issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
   before_action :configure_devise_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_devise_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, { tags: [] }, :age, :first_name, :description, photos: []])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, { tags: [] }, :age, :description, photos: []])
   end
 end
