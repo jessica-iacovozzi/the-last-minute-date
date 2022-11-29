cd6b0adf6d7425492258905f3040ae4bdf3ca570
jessica iacovozzi <iacovozzi.jessica@gmail.com>
2022-11-29 10:23:22 -0500

    permitted username on sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,14 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_devise_permitted_parameters, if: :devise_controller?
 
-  def configure_devise_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, { tags: [] }, :age, :description, photos: []])
-  end
-
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
+  protected
+
+  def configure_devise_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, { tags: [] }, :age, :description, photos: []])
   end
 end
