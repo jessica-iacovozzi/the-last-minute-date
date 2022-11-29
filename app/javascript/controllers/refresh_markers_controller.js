85054725919545ea929dc49e0747de57697966fa
jessica iacovozzi <iacovozzi.jessica@gmail.com>
2022-11-29 14:31:37 -0500

    styling the modal

--- app/javascript/controllers/refresh_markers_controller.js
+++ app/javascript/controllers/refresh_markers_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="refresh-markers"
 export default class extends Controller {
-  static targets = ["categories", "mapRefresh", "child", "modal"]
+  static targets = ["categories", "mapRefresh", "child", "modal", "form"]
 
   connect() {
 
@@ -16,15 +16,4 @@ export default class extends Controller {
       }
     }))
   }
-
-
-  // <script>$('#exampleModal').on('show.bs.modal', function (event) {
-  //   var button = $(event.relatedTarget) // Button that triggered the modal
-  //   var recipient = button.data('whatever') // Extract info from data-* attributes
-  //   // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-  //   // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-  //   var modal = $(this)
-  //   modal.find('.modal-title').text('New message to ' + recipient)
-  //   modal.find('.modal-body input').val(recipient)
-  // })</script>
 }
